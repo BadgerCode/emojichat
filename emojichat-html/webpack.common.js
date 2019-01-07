1ad2e2f572ecac1a43384d50f15385ea2c3111b0
Michael Hawkins <BadgerCode@users.noreply.github.com>
2019-01-07 23:41:18 +0000

    This might fix babel usage

--- emojichat-html/webpack.common.js
+++ emojichat-html/webpack.common.js
@@ -20,9 +20,7 @@ module.exports = {
             {
                 test: /\.js$/,
                 exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader'
-                }
+                use: 'babel-loader'
             }
         ]
     }
