2433e21ae94d564f0df2acac08dd3e7ff7fd8ad0
Michael Hawkins <BadgerCode@users.noreply.github.com>
2019-01-30 23:32:56 +0000

    Ensures the HTML is loaded as early as possible

--- emojichat/lua/autorun/emoji-chat.lua
+++ emojichat/lua/autorun/emoji-chat.lua
@@ -2,22 +2,26 @@ if SERVER then
     resource.AddWorkshop(1622781878)
 
     AddCSLuaFile()
+    AddCSLuaFile("emojichat/cl_echat.lua")
+    AddCSLuaFile("emojichat/cl_html.lua")
+
     AddCSLuaFile("emojichat/cl_chat-overrides.lua")
     AddCSLuaFile("emojichat/cl_hooks.lua")
     AddCSLuaFile("emojichat/cl_html-chat-callbacks.lua")
     AddCSLuaFile("emojichat/cl_html-chat-component.lua")
-    AddCSLuaFile("emojichat/cl_html.lua")
     AddCSLuaFile("emojichat/cl_init.lua")
     AddCSLuaFile("emojichat/cl_text-component.lua")
     AddCSLuaFile("emojichat/cl_util.lua")
 
     include("emojichat/sv_chat.lua")
 elseif CLIENT then
+    include("emojichat/cl_echat.lua")
+    include("emojichat/cl_html.lua")
+
     include("emojichat/cl_util.lua")
     include("emojichat/cl_text-component.lua")
     include("emojichat/cl_html-chat-component.lua")
     include("emojichat/cl_init.lua")
-    include("emojichat/cl_html.lua")
     include("emojichat/cl_chat-overrides.lua")
     include("emojichat/cl_hooks.lua")
     include("emojichat/cl_html-chat-callbacks.lua")
---
+++ emojichat/lua/emojichat/cl_echat.lua
@@ -0,0 +1,20 @@
+CHATMODE_GLOBAL = 1
+CHATMODE_TEAM = 2
+
+eChat = {
+	Ready = false,
+	Active = false,
+	ChatMode = CHATMODE_GLOBAL,
+	ExistingMessages = { },
+	HTMLOutput = nil
+}
+
+eChat.config = {
+	html = "Loading...",
+	timestamps = true,
+	position = 1,	
+	fadeTime = 12,
+	defaultChatColour = Color(255, 255, 255, 255),
+	timestampColour = Color(255, 255, 255, 255),
+	serverMessageColour = Color(151, 211, 255, 255)
+}
